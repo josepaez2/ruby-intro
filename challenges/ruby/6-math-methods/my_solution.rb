6a493107f9493bea67f2150f79d96400827bf626
josepaez2 <josepaez_2@hotmail.com>
2017-06-08 02:39:00 -0500

    Prueba 6 Lista

--- challenges/ruby/6-math-methods/my_solution.rb
+++ challenges/ruby/6-math-methods/my_solution.rb
@@ -2,10 +2,39 @@
 
 # Tu solucion abajo:
 
-def add(a,b)
-  a + b
+# def add(a,b)
+#   a + b
+# end
+
+# def modulus(a,b)
+#   a % b
+# end
+
+
+def add(number1, number2)
+number1 + number2
 end
 
-def modulus(a,b)
-  a % b
+def subtract(number1, number2)
+number1 +-number2
 end
+
+def multiply(number1, number2)
+number1 * number2
+end
+
+def divide(number1, number2)
+number1.to_f / number2.to_f
+end
+
+
+# Estos deberian aceptar tanto integers como numeros decimales como input. `divide` tiene que realizar la division usando `Floats`.
+
+# Por ejemplo,
+
+# ```ruby
+# add(10,2)      # => 12
+# subtract(10,2) # => 8
+# multiply(10,2) # => 20
+# divide(10,2)   # => 5.0 (*no* 5)
+# ```
