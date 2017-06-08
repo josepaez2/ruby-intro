4e691f30e04dec516685c56029786dda046314e8
josepaez2 <josepaez_2@hotmail.com>
2017-06-08 03:07:29 -0500

    Prueba 7 Lista

--- challenges/ruby/7-good-guess/my_solution.rb
+++ challenges/ruby/7-good-guess/my_solution.rb
@@ -2,10 +2,19 @@
 
 # Tu solucion abajo:
 
-def good_guess?(a)
-  if a == 42
-    true
-  else
-    false
-  end
+# def good_guess?(a)
+#   if a == 42
+#     true
+#   else
+#     false
+#   end
+# end
+
+
+def good_guess?(integer)
+	if integer == 42
+		true
+	else 
+		false
+	end
 end
