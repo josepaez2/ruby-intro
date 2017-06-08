69a2818ed90eb0950482af42e704b14d537abd4e
josepaez2 <josepaez_2@hotmail.com>
2017-06-08 02:28:15 -0500

    Prueba 5 Lista

--- challenges/ruby/5-format-address/my_solution.rb
+++ challenges/ruby/5-format-address/my_solution.rb
@@ -2,6 +2,10 @@
 
 # Tu solucion abajo:
 
-def make_address(street,city,state,zip)
-  "You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
+# def make_address(street,city,state,zip)
+#   "You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
+# end
+
+def make_address(myaddress, city, statecode, zipcode)
+"You live at #{myaddress}, in the beautiful city of #{city}, #{statecode}. Your zip is #{zipcode}."
 end
